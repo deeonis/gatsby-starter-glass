d5fed0feedfeb0898a8aaecb6ea35af20842d7ef
Denis Kuznetsov <kuznecov@MacBook-Pro-2.local>
2021-07-03 17:55:53 +0300

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -207,5 +207,14 @@ module.exports = {
         defer: false,
       },
     },
+     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-G-THLCSM7QFN" // Google Analytics / GA
+        ],
+      },
+    },
   ],
 };
